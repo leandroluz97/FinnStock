60a1e5ebfb706287855ba3e9dcd7daba22a0b113
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-26 12:29:54 +0100

    setup eslint with prettier and airbnb styleguide

--- FinnStock.Frontend/.eslintrc.cjs
+++ FinnStock.Frontend/.eslintrc.cjs
@@ -1,14 +1,21 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    env: { browser: true, es2021: true },
+    extends: [
+        'airbnb',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    plugins: ['react-refresh', 'prettier'],
+
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        'react/jsx-fragments': 0,
+        'import/no-unresolved': [2, { caseSensitive: false }],
+    },
+};
--- FinnStock.Frontend/vite.config.ts
+++ FinnStock.Frontend/vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import dns from "dns";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import dns from 'dns';
 
-dns.setDefaultResultOrder("verbatim");
+dns.setDefaultResultOrder('verbatim');
 
 // https://vitejs.dev/config/
 export default defineConfig({
