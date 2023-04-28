b3e6f436d90fc228e3c9ef35d796bb96520c4962
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-28 17:04:33 +0100

    added signup responsive form

--- FinnStock.Frontend/src/routes/index.tsx
+++ FinnStock.Frontend/src/routes/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import finnstockLogo from '../assets/finnstock-white.svg';
 import googleLogo from '../assets/google-logo-sm.svg';
+import { InputField } from '../components/form/InputField';
 
 export const AppRoutes = () => {
     // throw Error('error');
     return (
         <div className="h-screen">
             <div className="flex h-full">
-                <div className="flex-1 bg-primary-900 p-10 bg-logo-pattern bg-no-repeat bg-bottom bg-contain">
+                <div className=" hidden md:block  flex-1 bg-primary-900 p-10 bg-logo-pattern bg-no-repeat bg-bottom bg-contain">
                     <div>
                         <img src={finnstockLogo} alt="finnstock" />
                     </div>
@@ -20,8 +21,8 @@ export const AppRoutes = () => {
                     </div>
                 </div>
                 <div className="flex-1 bg-white">
-                    <div className="container max-w-lg m-auto p-10">
-                        <div className="text-right">
+                    <div className="container max-w-lg m-auto p-5 md:p-10">
+                        {/* <div className="text-right">
                             <a
                                 href="/login"
                                 type="button"
@@ -29,43 +30,45 @@ export const AppRoutes = () => {
                             >
                                 Login
                             </a>
-                        </div>
+                        </div> */}
 
                         <section className="">
-                            <h2 className="text-3xl font-medium text-primary-900 mb-4">Sign In</h2>
+                            <h2 className="text-3xl font-medium text-primary-900 mb-8">Sign In</h2>
                             <form action="" className="py-2">
-                                <div className="my-4">
-                                    <label
-                                        htmlFor="first_name"
-                                        className="block mb-1 text-sm font-medium text-primary-950"
-                                    >
-                                        First Name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        id="first_name"
-                                        className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 ring-2 focus:border-primary border-2 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                        placeholder="John"
-                                        required
-                                    />
+                                <div className=" flex flex-col lg:flex-row gap-3">
+                                    <div className="flex-1">
+                                        <label
+                                            htmlFor="first_name"
+                                            className="block mb-1 text-sm font-medium text-primary-950"
+                                        >
+                                            First Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="first_name"
+                                            className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
+                                            // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-primary-500 ring-2 focus:border-primary border-2 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                            placeholder="John"
+                                            required
+                                        />
+                                    </div>
+                                    <div className="flex-1">
+                                        <label
+                                            htmlFor="first_name"
+                                            className="block mb-1 text-sm font-medium text-primary-950"
+                                        >
+                                            Last Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="first_name"
+                                            className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
+                                            placeholder="John"
+                                            required
+                                        />
+                                    </div>
                                 </div>
-                                <div className="my-4">
-                                    <label
-                                        htmlFor="first_name"
-                                        className="block mb-1 text-sm font-medium text-primary-950"
-                                    >
-                                        Last Name
-                                    </label>
-                                    <input
-                                        type="text"
-                                        id="first_name"
-                                        className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        placeholder="John"
-                                        required
-                                    />
-                                </div>
-                                <div className="my-4">
+                                <div className="my-4 flex-1">
                                     <label
                                         htmlFor="first_name"
                                         className="block mb-1 text-sm font-medium text-primary-950"
@@ -74,15 +77,32 @@ export const AppRoutes = () => {
                                     </label>
                                     <input
                                         type="text"
-                                        id="first_name"
-                                        className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        placeholder="John"
+                                        id="email"
+                                        // className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
+                                        className="bg-red-50 border-red-200 border-2 text-red-700 text-sm rounded focus:ring-red-100 focus:ring-2 focus:border-red-600 focus:border-2 block w-full p-2.5 placeholder-red-400"
+                                        placeholder="johndoe@gmail.com"
                                         required
                                     />
+                                    <p className="mt-2 text-xs text-red-600 dark:text-red-500">
+                                        <span className="font-medium">Well done!</span> Some success
+                                        message.
+                                    </p>
                                 </div>
+                                <InputField
+                                    id="email"
+                                    name="email"
+                                    label="Email"
+                                    placeholder="johndoe@gmail.com"
+                                    type="text"
+                                    groupFormClassList="my-4 flex-1"
+                                    hasError={false}
+                                    isRequired
+                                    errorMessage=""
+                                    key="email"
+                                />
                                 <div className="my-4">
                                     <label
-                                        htmlFor="first_name"
+                                        htmlFor="phone_number"
                                         className="block mb-1 text-sm font-medium text-primary-950"
                                     >
                                         Phone number
@@ -91,7 +111,7 @@ export const AppRoutes = () => {
                                         type="text"
                                         id="first_name"
                                         className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        placeholder="John"
+                                        placeholder="915210066"
                                         required
                                     />
                                 </div>
@@ -103,25 +123,9 @@ export const AppRoutes = () => {
                                         Password
                                     </label>
                                     <input
-                                        type="text"
+                                        type="password"
                                         id="first_name"
                                         className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        placeholder="John"
-                                        required
-                                    />
-                                </div>
-                                <div className="my-4">
-                                    <label
-                                        htmlFor="first_name"
-                                        className="block mb-1 text-sm font-medium text-primary-950"
-                                    >
-                                        Confirm password
-                                    </label>
-                                    <input
-                                        type="text"
-                                        id="first_name"
-                                        className="bg-primary-50 border-primary-200 border-2 text-primary-900 text-sm rounded focus:ring-primary-100 focus:ring-2 focus:border-primary-800 focus:border-2 block w-full p-2.5 placeholder-primary-400"
-                                        placeholder="John"
                                         required
                                     />
                                 </div>
