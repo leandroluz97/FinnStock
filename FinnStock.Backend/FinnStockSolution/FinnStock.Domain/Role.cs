1a44869ef843f0020ba5c76d014b47da02f84710
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-29 20:03:13 +0100

    Added Role and User Domain

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Domain/Role.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Domain
+{
+    public class Role : IdentityRole<int>
+    {
+        public Guid GlobalId { get; set; }
+    }
+}
---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Domain/User.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Domain
+{
+    public class User :  IdentityUser<int>
+    {
+        public Guid GlobalId { get; set; }
+    }
+}
