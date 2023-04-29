87af4eda2349cfe43b169713d87b6e713176b483
leandroluz97 <leandrodaluz97@gmail.com>
2023-04-30 00:29:26 +0100

    added repository base interface

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Infrastructure.Abstractions/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Infrastructure.Abstractions.Repositories
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        T Get(Expression<Func<T, bool>> expression);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateRange(IEnumerable<T> entities);
+        Task<T> GetAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default);
+        Task AddAsync(T entity, CancellationToken cancellationToken = default);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default);
+    }
+}
---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.UnitOfWork/Repositories/BaseRepository.cs
@@ -0,0 +1,101 @@
+﻿using FinnStock.Infrastructure.Abstractions.Repositories;
+using FinnStock.SQLWork;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.UnitOfWork.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<T> _dbSet;
+
+        public BaseRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
+        }
+
+
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public async Task AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+             await _dbContext.AddAsync(entity, cancellationToken);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            _dbContext.AddRange(entities);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.AddRangeAsync(entities, cancellationToken);
+        }
+
+        public T Get(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.FirstOrDefault(expression);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.AsEnumerable();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression).AsEnumerable();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.ToListAsync(cancellationToken);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.Where(expression).ToListAsync(cancellationToken);
+        }
+
+        public async Task<T> GetAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FirstOrDefaultAsync(expression, cancellationToken);
+        }
+
+        public async Task<T> GetAsync(CancellationToken cancellationToken = default)
+        {
+            return await _dbSet.FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbContext.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            _dbContext.RemoveRange(entities);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<T> entities)
+        {
+            _dbContext.UpdateRange(entities);
+        }
+    }
+
+}
