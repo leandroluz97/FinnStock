ea8985531b7e480d1ad929e5e925834128dce220
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-04 00:03:40 +0100

    added first and last name to user domain

--- FinnStock.Backend/FinnStockSolution/FinnStock.Domain/User.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Domain/User.cs
@@ -10,6 +10,8 @@ namespace FinnStock.Domain
 {
     public class User :  IdentityUser<Guid>
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public ICollection<Order> Orders { get; set; }
         public ICollection<Favorite> Favorites { get; set; }
         public string? RefreshToken { get; set; }
