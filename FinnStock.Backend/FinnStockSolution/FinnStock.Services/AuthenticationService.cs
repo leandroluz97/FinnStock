5c128e64589bb289b0cdcd8cb538347e6d88fbf0
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-04 00:04:07 +0100

    added service NotFoundException

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/AuthenticationService.cs
@@ -0,0 +1,77 @@
+﻿using FinnStock.Business.Abstractions;
+using FinnStock.Domain;
+using FinnStock.Dtos;
+using FinnStock.Services.Exceptions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Services
+{
+    public class AuthenticationService : IAuthorizationService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<AuthenticationService> _logger;
+
+        public AuthenticationService(ILogger<AuthenticationService> logger, UserManager<User> userManager )
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+        public async Task Register(RegisterDto registerDto)
+        {
+
+            var user = new User()
+            {
+                Email = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber,
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            //TODO: sent email confirmation via SendGrid email 
+
+            throw new NotImplementedException();
+        }
+        public Task Login()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Task ResetPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TwoWayFactorAuthentication()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ConfirmEmail(string token, string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+
+            if(user == null)
+            {
+                throw new NotFoundException($"{nameof(user)} with {nameof(email)} not found");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, token);
+
+            if (!result.Succeeded)
+            {
+                throw new Exception();
+            }
+            //TODO: Generate JWT
+        }
+    }
+}
