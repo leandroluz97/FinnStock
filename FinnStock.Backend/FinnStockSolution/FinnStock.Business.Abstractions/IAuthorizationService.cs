38a48ae443ec0825143140a92a2a7fe8757600c8
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-04 00:02:56 +0100

    updated authorization service

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Business.Abstractions/IAuthorizationService.cs
@@ -0,0 +1,18 @@
+﻿using FinnStock.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Business.Abstractions
+{
+    public interface IAuthorizationService
+    {
+        public Task Register(RegisterDto registerDto);
+        public Task ConfirmEmail(string token, string email);
+        public Task  Login();
+        public Task  TwoWayFactorAuthentication();
+        public Task  ResetPassword();
+    }
+}
