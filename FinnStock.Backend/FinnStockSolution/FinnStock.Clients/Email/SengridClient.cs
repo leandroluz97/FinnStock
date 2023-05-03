4d1c8a7abdbc97f95162dd52c133e11e769eb4c0
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-04 00:04:45 +0100

    implemented sendGrid mailing client

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Clients/Email/SengridClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinnStock.Infrastructure.Abstractions.Clients;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
+namespace FinnStock.Clients.Email
+{
+    public class SengridClient : EmailClient
+    {
+        private readonly IConfiguration _configuration;
+        public SengridClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendAccountConfirmationAsync(string recipientEmail, string confirmationLink)
+        {
+            var client = new SendGridClient("");
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("leandroluz97@gmail.com", "Leandro Luz"),
+                ReplyTo = new EmailAddress(recipientEmail),
+                Subject = "Confirm your email address",
+                HtmlContent = $"<p>This is your confirmation link: <a href={confirmationLink} ></a></p>"
+            };
+
+            var response = await client.SendEmailAsync(msg);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Couldn't send {nameof(recipientEmail)} account confirmation email.");
+            }
+        }
+    }
+}
---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Infrastructure.Abstractions/Clients/EmailClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Infrastructure.Abstractions.Clients
+{
+    public interface EmailClient
+    {
+        public Task SendAccountConfirmationAsync(string recipientEmail, string confirmationLink);
+    }
+}
