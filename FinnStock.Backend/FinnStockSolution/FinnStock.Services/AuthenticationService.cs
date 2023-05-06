5452430f48fac8af0c8395bbb0691c9064d43cd5
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-06 23:17:18 +0100

    added Authentication otp code validation method

--- FinnStock.Backend/FinnStockSolution/FinnStock.Services/AuthenticationService.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/AuthenticationService.cs
@@ -4,10 +4,14 @@ using FinnStock.Dtos;
 using FinnStock.Infrastructure.Abstractions.Clients;
 using FinnStock.Services.Exceptions;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,18 +19,35 @@ namespace FinnStock.Services
 {
     public class AuthenticationService 
     {
-        private readonly UserManager<User> _userManager;
         private readonly ILogger<AuthenticationService> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
         private readonly IEmailClient _sendgridClient;
+        private readonly IMessageClient _twilioClient;
 
-        public AuthenticationService(ILogger<AuthenticationService> logger, UserManager<User> userManager, IEmailClient sendgridClient)
+        public AuthenticationService(
+            ILogger<AuthenticationService> logger, 
+            IConfiguration configuration, 
+            UserManager<User> userManager,
+            SignInManager<User> signInManager,
+            IEmailClient sendgridClient,
+            IMessageClient twilioClient)
         {
             _logger = logger;
+            _configuration = configuration;
             _userManager = userManager;
+            _signInManager = signInManager;
             _sendgridClient = sendgridClient;
+            _twilioClient = twilioClient;
         }
         public async Task Register(RegisterDto registerDto)
         {
+            if(registerDto == null)
+            {
+                throw new ArgumentNullException(nameof(registerDto));
+            }
+            registerDto.EnsureValidation();
 
             var user = new User()
             {
@@ -40,50 +61,118 @@ namespace FinnStock.Services
 
             if (!result.Succeeded)
             {
-                throw new Exception(result.Errors.ToList().ToString());
+                throw new InvalidOperationException(result.Errors.ToList().ToString());
             }
 
             var token = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
             var uriBuilder = new UriBuilder("https://localhost:3000/auth/confirm");
-            uriBuilder.Query = $"token={token}";
+            uriBuilder.Query = $"token={token}&email={user.Email}";
             var confirmationLink = uriBuilder.Uri.ToString();
 
             await _sendgridClient.SendAccountConfirmationAsync(user.Email, confirmationLink);
     
         }
-        public Task Login()
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public Task ResetPassword()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task TwoWayFactorAuthentication()
-        {
-            throw new NotImplementedException();
-        }
 
         public async Task ConfirmEmail(string token, string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
-
-            if(user == null)
+            if (user == null)
             {
-                throw new NotFoundException($"{nameof(user)} with {nameof(email)} not found");
+                throw new NotFoundException($"{nameof(user)} with {email} not found");
             }
 
             var result = await _userManager.ConfirmEmailAsync(user, token);
 
             if (!result.Succeeded)
             {
-                throw new Exception();
+                throw new InvalidOperationException();
             }
-            //TODO: Generate JWT
         }
+
+        public async Task Login(LoginDto loginDto)
+        {
+            if (loginDto == null)
+            {
+                throw new ArgumentNullException(nameof(loginDto));
+            }
+            loginDto.EnsureValidation();
+
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null)
+            {
+                throw new NotFoundException($"{nameof(user)} with {loginDto.Email} not found");
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginDto.Email, loginDto.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.RequiresTwoFactor)
+            {
+                var code = await _userManager.GenerateTwoFactorTokenAsync(user, "Phone");
+                var message = "Your security code is: " + code;
+                await _twilioClient.SendOTPMessageAsync(user.PhoneNumber, message);
+            }
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException("Unable to load two-factor authentication user.");
+            }
+          
+        }
+
+        public async Task<ResponseToken> ValidateOTP(string otpCode, string userId)
+        {
+           if(otpCode == null)
+            {
+                throw new ArgumentNullException(nameof(otpCode));
+            }
+            var result = await _signInManager.TwoFactorSignInAsync("Phone", otpCode, isPersistent:true, rememberClient:true);
+            if (!result.Succeeded)
+            {
+                throw new InvalidOperationException($"Invalid {nameof(otpCode)}");
+            }
+            
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return CreateJWToken(user);
+        }
+
+        public async Task ResetPassword()
+        {
+           
+            throw new NotImplementedException();
+        }
+
+        private ResponseToken CreateJWToken(User user)
+        {
+            DateTime expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:Exp_time"]));
+            Claim[] claims = new Claim[] {
+                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), //Subject (userId)
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //JWT unique ID
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), //Issued at (date and timeof token generation)
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), //Unit name Identifier of the user (Id or email)
+            };
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret_key"]));
+
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            JwtSecurityToken tokenGenerator = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: expiration,
+                signingCredentials: signingCredentials
+                );
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.WriteToken(tokenGenerator);
+
+
+            return new ResponseToken() { Token = token };
+           
+        }
+
+
+
+
     }
 }
--- FinnStock.Backend/FinnStockSolution/FinnStock.Services/JwtService.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/JwtService.cs
@@ -19,67 +19,70 @@ namespace FinnStock.Services
         }
         public ResponseToken CreateJWToken(User user)
         {
-            DateTime expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:Exp_time"]));
-            Claim[] claims = new Claim[] {
-                new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), //Subject (userId)
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //JWT unique ID
-                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), //Issued at (date and timeof token generation)
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), //Unit name Identifier of the user (Id or email)
-            };
+            //DateTime expiration = DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["Jwt:Exp_time"]));
+            //Claim[] claims = new Claim[] {
+            //    new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()), //Subject (userId)
+            //    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()), //JWT unique ID
+            //    new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()), //Issued at (date and timeof token generation)
+            //    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()), //Unit name Identifier of the user (Id or email)
+            //};
 
-            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret_key"]));
+            //SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Secret_key"]));
 
-            SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            //SigningCredentials signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
-            JwtSecurityToken tokenGenerator = new JwtSecurityToken(
-                _configuration["Jwt:Issuer"],
-                _configuration["Jwt:Audience"],
-                claims,
-                expires: expiration,
-                signingCredentials: signingCredentials
-                );
+            //JwtSecurityToken tokenGenerator = new JwtSecurityToken(
+            //    _configuration["Jwt:Issuer"],
+            //    _configuration["Jwt:Audience"],
+            //    claims,
+            //    expires: expiration,
+            //    signingCredentials: signingCredentials
+            //    );
 
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-            var token = tokenHandler.WriteToken(tokenGenerator);
+            //JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            //var token = tokenHandler.WriteToken(tokenGenerator);
 
 
-            return new ResponseToken() { 
-                Token = token, 
-                RefreshToken = GenerateRefreshToken(),
-                RefreshTokenExpirationTime = DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["RefreshToken:Exp_time"]))
-            };
+            //return new ResponseToken() { 
+            //    Token = token, 
+            //    RefreshToken = GenerateRefreshToken(),
+            //    RefreshTokenExpirationTime = DateTime.Now.AddMinutes(Convert.ToInt32(_configuration["RefreshToken:Exp_time"]))
+            //};
+            throw new NotImplementedException();
         }
 
 
         private string GenerateRefreshToken()
         {
-            var bytes = new byte[64];
-            var randomNumberGenerator = RandomNumberGenerator.Create();
-            randomNumberGenerator.GetBytes(bytes);
-            return Convert.ToBase64String(bytes);
+            //var bytes = new byte[64];
+            //var randomNumberGenerator = RandomNumberGenerator.Create();
+            //randomNumberGenerator.GetBytes(bytes);
+            //return Convert.ToBase64String(bytes);
+            throw new NotImplementedException();
         }
         public ClaimsPrincipal? GetPrincipalFromJwt(string? token)
         {
-            var tokenValidationParameters = new TokenValidationParameters()
-            {
-                ValidateAudience = true,
-                ValidAudience = _configuration["Jwt:Audience"],
-                ValidateIssuer = true,
-                ValidIssuer = _configuration["Jwt:Issuer"],
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:Secret_key"])),
-                ValidateLifetime = false, //Should Be false as the Jwtoken is already expired
-            };
+            //var tokenValidationParameters = new TokenValidationParameters()
+            //{
+            //    ValidateAudience = true,
+            //    ValidAudience = _configuration["Jwt:Audience"],
+            //    ValidateIssuer = true,
+            //    ValidIssuer = _configuration["Jwt:Issuer"],
+            //    ValidateIssuerSigningKey = true,
+            //    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:Secret_key"])),
+            //    ValidateLifetime = false, //Should Be false as the Jwtoken is already expired
+            //};
 
-            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
-            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            //JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            //var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
 
-            if(securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-            {
-                throw new SecurityTokenException("Invalid token");
-            }
+            //if(securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            //{
+            //    throw new SecurityTokenException("Invalid token");
+            //}
 
-            return principal;
+            //return principal;
+            throw new NotImplementedException();
         }
     }
 }
