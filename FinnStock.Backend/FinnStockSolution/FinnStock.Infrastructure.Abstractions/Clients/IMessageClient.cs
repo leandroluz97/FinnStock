b963a27660708f9042333d6d4862a10038d878f1
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-06 23:17:41 +0100

    added IMessageClient interface

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Infrastructure.Abstractions/Clients/IMessageClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Infrastructure.Abstractions.Clients
+{
+    public interface IMessageClient
+    {
+        public Task SendOTPMessageAsync(string number, string message);
+    }
+}
