4a8a39f4751946dccc55a73c54e316d5c4e4dbbf
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-07 23:28:44 +0100

    added confirmEmail, LoginDto and ValidateDto

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/ConfirmEmailDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Dtos
+{
+    public class ConfirmEmailDto
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrEmpty(Token))
+            {
+                throw new ArgumentException(nameof(Token));
+            }
+            if (string.IsNullOrEmpty(Email))
+            {
+                throw new ArgumentException(nameof(Email));
+            }
+        }
+    }
+}
---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/ValidateOtpDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Dtos
+{
+    public class ValidateOtpDto
+    {
+        public string OtpCode { get; set; }
+        public string UserId { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (string.IsNullOrEmpty(OtpCode))
+            {
+                throw new ArgumentException(nameof(OtpCode));
+            }
+            if (string.IsNullOrEmpty(UserId))
+            {
+                throw new ArgumentException(nameof(UserId));
+            }
+        }
+    }
+}
