8778a93997f1ccaed7009940f8873596a252c2d7
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-16 23:06:45 +0100

    implemented SearchStocksAsync method

--- FinnStock.Backend/FinnStockSolution/FinnStock.Clients/Finnhub/FinnhubClient.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Clients/Finnhub/FinnhubClient.cs
@@ -23,7 +23,8 @@ namespace FinnStock.Clients.Finnhub
             _httpClient.BaseAddress = new Uri("https://finnhub.io/api/v1");
         }
 
-        public async Task<IEnumerable<StockDto>> GetStocks()
+
+        public async Task<IEnumerable<StockDto>> GetStocksAsync()
         {
             var response = await _httpClient.GetAsync("/stock/symbol?exchange=US");
 
@@ -32,6 +33,32 @@ namespace FinnStock.Clients.Finnhub
             var responseJson = await response.Content.ReadAsStringAsync();
 
             var responseData = JsonSerializer.Deserialize<IEnumerable<StockDto>>(responseJson, _jsonSerializerOptions);
+
+            return responseData;
+        }
+
+        public async Task<IEnumerable<SymbolDto>> SearchStocksAsync(string searchText)
+        {
+            var response = await _httpClient.GetAsync($"/search?q={searchText}");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            var responseData = JsonSerializer.Deserialize<IEnumerable<SymbolDto>>(responseJson, _jsonSerializerOptions);
+
+            return responseData;
+        }
+
+        public async Task<CompanyDto> GetCompaniesAsync(string symbol)
+        {
+            var response = await _httpClient.GetAsync($"/stock/profile2?symbol={symbol}");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseJson = await response.Content.ReadAsStringAsync();
+
+            var responseData = JsonSerializer.Deserialize<CompanyDto>(responseJson, _jsonSerializerOptions);
 
             return responseData;
         }
