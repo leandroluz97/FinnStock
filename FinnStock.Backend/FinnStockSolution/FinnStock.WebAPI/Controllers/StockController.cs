b83f3b9982fce710c9abb464b955f828397c4fa9
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-19 02:04:40 +0100

    Added Stock controller

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.WebAPI/Controllers/StockController.cs
@@ -0,0 +1,29 @@
+﻿using FinnStock.Dtos;
+using FinnStock.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinnStock.WebAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ILogger<StockController> _logger;
+        private readonly StockService _stockService;
+        public StockController(ILogger<StockController> logger, StockService stockService)
+        {
+            _logger = logger;
+            _stockService = stockService;
+        }
+
+        [HttpGet]
+        [Route("stocks")]
+        public async Task<IEnumerable<StockDto>> Stocks()
+        {
+           var stocks =  await _stockService.GetAllAsync();
+           return stocks; 
+        }
+
+    }
+}
