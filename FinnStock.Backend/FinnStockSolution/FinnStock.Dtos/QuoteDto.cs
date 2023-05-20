470dfb54b4887d78b5df9d786e8981120c947a2c
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-20 15:48:52 +0100

    Added Quote Dto

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/QuoteDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Dtos
+{
+    public class QuoteDto
+    {
+        public string C { get; set; }
+        public string H{ get; set; }
+        public string L { get; set; }
+        public string O { get; set; } 
+        public string Pc { get; set; }
+        public string T { get; set; }
+    }
+}
+
