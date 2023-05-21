187996b814211065c1fba3ca4b5243e4e294101c
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-21 02:56:49 +0100

    added pagination class

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Utils/Pagination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Utils
+{
+    public class Pagination<T>
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public int TotalPages { get; set; }
+        public IEnumerable<T> Items { get; set; }
+    }
+}
