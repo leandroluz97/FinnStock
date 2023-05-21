c1b4bb0a9fd03de3de9955dd8c9689cbe9fdf72f
leandroluz97 <leandrodaluz97@gmail.com>
2023-05-21 02:54:17 +0100

    added BuyOrder and SellOrder DTOs

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/BuyOrderDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Dtos
+{
+    public class BuyOrderDto
+    {
+        public Guid Id { get; set; }
+        public double Amount { get; set; }
+        public int Quantity { get; set; }
+        public string Symbol { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; } 
+
+        public void EnsureValidation()
+        {
+            if (Amount == default)
+            {
+                throw new ArgumentException(nameof(Amount));
+            }
+
+            if (Quantity == default)
+            {
+                throw new ArgumentException(nameof(Quantity));
+            }
+            
+            if (string.IsNullOrEmpty(Symbol))
+            {
+                throw new ArgumentException(nameof(Symbol));
+            }
+            
+            if (UserId == default)
+            {
+                throw new ArgumentException(nameof(UserId));
+            }
+
+        }
+    }
+}
