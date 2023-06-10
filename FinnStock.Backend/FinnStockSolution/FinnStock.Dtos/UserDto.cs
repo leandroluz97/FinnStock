88691b9fe7959c46474737a0e0f858996f0a81d6
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-10 18:36:27 +0100

    added userDto

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/UserDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Dtos
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; } 
+        public DateTime? BirthDate { get; set; } 
+        public string? ProfileId { get; set; } 
+    }
+}
