27ee932cdcbb1d51e2822c15a712fb5ffdcccdb6
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-11 00:12:39 +0100

    Added userSservices

--- FinnStock.Backend/FinnStockSolution/FinnStock.Services/UserService.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿using FinnStock.Domain;
 using FinnStock.Dtos;
+using FinnStock.Services.Exceptions;
+using FinnStock.Services.Mappers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using System;
@@ -20,22 +22,72 @@ namespace FinnStock.Services
             _userManager = userManager;
         }
 
-        public async Task<UserDto> GetByIdAsync()
+        public async Task<UserDto> GetByIdAsync(Guid userId, CancellationToken cancellationToken = default)
         {
+            if (userId == default)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
 
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+
+            if(user == null)
+            {
+                throw new NotFoundException(nameof(userId));
+            }
+
+            return UserMapper.ToDto(user);
         }
-        public async Task<UserDto> UpdateAsync()
-        {
 
+        public async Task<UserDto> UpdateAsync(Guid userId, UserDto user, CancellationToken cancellationToken = default)
+        {
+            if(userId == default)
+            {
+                throw new ArgumentNullException(nameof(userId));
+            }
+
+            if(user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+            user.EnsureValidation();
+
+            var  userDomain = await _userManager.FindByIdAsync(userId.ToString());
+
+            if(userDomain == null)
+            {
+                throw new NotFoundException(nameof(userDomain));
+            }
+
+            userDomain.FirstName = user.FirstName;
+            userDomain.LastName = user.LastName;
+            userDomain.BirthDate = user.BirthDate;
+            userDomain.ProfileId = user.ProfileId;
+
+            var updatedUserResponse = await _userManager.UpdateAsync(userDomain);
+
+            if (!updatedUserResponse.Succeeded)
+            {
+                throw new InvalidOperationException($"Could not pdate user {userDomain.Id}");
+            }
+
+            return UserMapper.ToDto(userDomain);
         }
 
         public async Task<UserDto> GetProfileImageAsync()
         {
 
         }
-        public async Task<UserDto> UploadProfileImageAsync()
+        public async Task<byte[]> UploadProfileImageAsync(Guid userId, byte[] bytes, CancellationToken cancellationToken = default)
         {
+            if(bytes == null)
+            {
+                throw new ArgumentNullException(nameof(bytes));
+            }
 
+            var fileToBase64 = Convert.ToBase64String(bytes);
+
+            return Convert.FromBase64String(fileToBase64);  
         }
 
     }
