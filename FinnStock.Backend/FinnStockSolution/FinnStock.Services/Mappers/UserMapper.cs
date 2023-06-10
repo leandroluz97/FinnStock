82c9f857ddaad2625560e04e3c35e9f67d922f5b
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-11 00:12:17 +0100

    added userMapper

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/Mappers/UserMapper.cs
@@ -0,0 +1,40 @@
+﻿using FinnStock.Domain;
+using FinnStock.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Services.Mappers
+{
+    public class UserMapper
+    {
+        public static User ToDomain(UserDto dto)
+        {
+            return new User()
+            {
+                FirstName = dto.FirstName,
+                LastName = dto.LastName,
+                Email = dto.Email,
+                PhoneNumber = dto.PhoneNumber,
+                ProfileId = dto.ProfileId,
+                BirthDate = dto.BirthDate,
+            };
+        }
+
+        public static UserDto ToDto(User domain)
+        {
+            return new UserDto()
+            {
+                Id = domain.Id,
+                FirstName = domain.FirstName,
+                LastName = domain.LastName,
+                Email = domain.Email,
+                PhoneNumber = domain.PhoneNumber,
+                ProfileId = domain.ProfileId,
+                BirthDate = domain.BirthDate,
+            };
+        }
+    }
+}
