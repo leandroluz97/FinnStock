7c56e9b49dd552f82c3f797fa143ce4dbbeb6839
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-10 18:37:00 +0100

    added userService class to handle user crud operations

---
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using FinnStock.Domain;
+using FinnStock.Dtos;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinnStock.Services
+{
+    public class UserService
+    {
+        private readonly ILogger<UserService> _logger;
+        private readonly UserManager<User> _userManager;
+        public UserService(ILogger<UserService> logger, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public async Task<UserDto> GetByIdAsync()
+        {
+
+        }
+        public async Task<UserDto> UpdateAsync()
+        {
+
+        }
+
+        public async Task<UserDto> GetProfileImageAsync()
+        {
+
+        }
+        public async Task<UserDto> UploadProfileImageAsync()
+        {
+
+        }
+
+    }
+}
