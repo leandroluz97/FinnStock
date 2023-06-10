a3bcee4818f48f2b90d01c45cb4aab40fa75374e
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-11 00:13:08 +0100

    added userController

--- FinnStock.Backend/FinnStockSolution/FinnStock.WebAPI/Controllers/UsersController.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.WebAPI/Controllers/UsersController.cs
@@ -30,5 +30,24 @@ namespace FinnStock.WebAPI.Controllers
         {
             return await _userService.UpdateAsync(userId, user);
         }
+
+        [HttpPost]
+        [Route("{userId}/profile")]
+        public async Task<UserDto> UploadAsync(Guid userId)
+        {
+            IFormFile file = Request.Form.Files[0];
+            await using var memoryStream = new MemoryStream();
+            await file.CopyToAsync(memoryStream);
+            var bytes = memoryStream.ToArray();
+ 
+            return await _userService.UploadProfileImageAsync(userId, bytes);
+        }
+
+        [HttpPost]
+        [Route("{userId}/profile/{profileId}")]
+        public async Task<UserDto> UploadAsync(Guid userId, Guid profileId)
+        {
+            return await _userService.UpdateAsync(userId, user);
+        }
     }
 }
