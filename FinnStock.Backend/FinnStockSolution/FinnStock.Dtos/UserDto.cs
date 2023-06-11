911d30d0c9c56a7e87be52c32dda64d86224b194
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-11 23:07:45 +0100

    commented profileId

--- FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/UserDto.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.Dtos/UserDto.cs
@@ -11,8 +11,25 @@ namespace FinnStock.Dtos
         public Guid Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string Email { get; set; } 
         public DateTime? BirthDate { get; set; } 
-        public string? ProfileId { get; set; } 
+        //public string? ProfileId { get; set; }
+
+        public void EnsureValidation()
+        {
+            if (Id == default)
+            {
+                throw new ArgumentException(nameof(Id));
+            }
+            if (string.IsNullOrWhiteSpace(PhoneNumber))
+            {
+                throw new ArgumentException(nameof(PhoneNumber));
+            }
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                throw new ArgumentException(nameof(Email));
+            }
+        }
     }
 }
