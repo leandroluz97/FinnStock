8abcb4807ff6a4193fece2ef1f1328e47eef5df0
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-13 19:12:34 +0100

    added stocks  controller pageNumber, pageSize

--- FinnStock.Backend/FinnStockSolution/FinnStock.WebAPI/Controllers/StocksController.cs
+++ FinnStock.Backend/FinnStockSolution/FinnStock.WebAPI/Controllers/StocksController.cs
@@ -23,12 +23,19 @@ namespace FinnStock.WebAPI.Controllers
 
         [HttpGet]
         [Route("")]
-        public async Task<Pagination<StockDto>> Stocks()
+        public async Task<Pagination<StockDto>> Stocks(int pageSize = 100, int pageNumber = 1)
         {
-           var stocks =  await _stockService.GetAllAsync();
+           var stocks =  await _stockService.GetAllAsync(pageNumber, pageSize);
            return stocks; 
         }
-        
+
+        [HttpGet]
+        [Route("")]
+        public async Task<Pagination<NewsDto>> MarketNews(int pageSize = 100, int pageNumber = 1)
+        {
+            return await _stockService.GetMarketNewsAsync(pageNumber, pageSize);
+        }
+
         [HttpGet]
         [Route("{symbol}/profile")]
         public async Task<CompanyDto> GetCompanyAsync(string symbol)
