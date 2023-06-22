e3796fc2fa42cb7633748e98cbb506417bfb10d7
leandroluz97 <leandrodaluz97@gmail.com>
2023-06-22 23:14:16 +0100

    added link url queries to bbuyorders tables

--- FinnStock.Frontend/src/features/orders/components/BuyOrdersTable.tsx
+++ FinnStock.Frontend/src/features/orders/components/BuyOrdersTable.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
 import { Search } from './Search';
+import { URLSearch } from '../../../utils/URLSearch';
 
 export const BuyOrdersTable = () => {
+    const QUERIES = URLSearch.queries();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const desc = QUERIES.sortDesc === 'true' ? 'false' : 'true';
+
     return (
         <React.Fragment>
             <Search />
@@ -11,9 +17,14 @@ export const BuyOrdersTable = () => {
                         <tr>
                             <th scope="col" className="px-6 py-3" />
                             <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Stock
-                                    <a href="#">
+                                <Link
+                                    to={URLSearch.set({
+                                        sortBy: 'stock',
+                                        sortDesc: QUERIES.sortBy === 'stock' ? desc : 'false',
+                                    })}
+                                >
+                                    <div className="flex items-center">
+                                        Stock
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             className="w-3 h-3 ml-1"
@@ -23,13 +34,18 @@ export const BuyOrdersTable = () => {
                                         >
                                             <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
                                         </svg>
-                                    </a>
-                                </div>
+                                    </div>
+                                </Link>
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Amount
-                                    <a href="#">
+                                <Link
+                                    to={URLSearch.set({
+                                        sortBy: 'amount',
+                                        sortDesc: QUERIES.sortBy === 'amount' ? desc : 'false',
+                                    })}
+                                >
+                                    <div className="flex items-center">
+                                        Amount
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             className="w-3 h-3 ml-1"
@@ -39,13 +55,18 @@ export const BuyOrdersTable = () => {
                                         >
                                             <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
                                         </svg>
-                                    </a>
-                                </div>
+                                    </div>
+                                </Link>
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Total
-                                    <a href="#">
+                                <Link
+                                    to={URLSearch.set({
+                                        sortBy: 'total',
+                                        sortDesc: QUERIES.sortBy === 'total' ? desc : 'false',
+                                    })}
+                                >
+                                    <div className="flex items-center">
+                                        Total
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             className="w-3 h-3 ml-1"
@@ -55,13 +76,18 @@ export const BuyOrdersTable = () => {
                                         >
                                             <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
                                         </svg>
-                                    </a>
-                                </div>
+                                    </div>
+                                </Link>
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                <div className="flex items-center">
-                                    Transaction Date
-                                    <a href="#">
+                                <Link
+                                    to={URLSearch.set({
+                                        sortBy: 'date',
+                                        sortDesc: QUERIES.sortBy === 'date' ? desc : 'false',
+                                    })}
+                                >
+                                    <div className="flex items-center">
+                                        Transaction Date
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             className="w-3 h-3 ml-1"
@@ -71,12 +97,131 @@ export const BuyOrdersTable = () => {
                                         >
                                             <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
                                         </svg>
-                                    </a>
-                                </div>
+                                    </div>
+                                </Link>
                             </th>
                         </tr>
                     </thead>
                     <tbody>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple
+                            </th>
+                            <th
+                                scope="row"
+                                className="px-6 py-4 font-medium text-primary-900 whitespace-nowrap dark:text-white"
+                            >
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td className="px-6 py-4">Silver</td>
+                            <td className="px-6 py-4">Laptop</td>
+                            <td className="px-6 py-4">$2999</td>
+                        </tr>
                         <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th
                                 scope="row"
