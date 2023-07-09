b4d59f8bb3308d70a86b69d90fd0ede0cd9e5446
leandroluz97 <leandrodaluz97@gmail.com>
2023-07-09 18:50:52 +0100

    REMOVED EMAJO

--- FinnStock.Frontend/src/components/error/ErrorFallback.tsx
+++ FinnStock.Frontend/src/components/error/ErrorFallback.tsx
@@ -6,7 +6,7 @@ export const ErrorFallback = () => {
             className="text-red-500 w-screen h-screen flex flex-col justify-center items-center"
             role="alert"
         >
-            <h2 className="text-lg font-semibold">Ooops, something went wrong :( </h2>
+            <h2 className="text-lg font-semibold">Ooops, something went wrong </h2>
             {/* <Button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
                 Refresh
             </Button> */}
