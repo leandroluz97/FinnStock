7ea9db765ca197ebe27c5a3b906bfa99f949b2b5
leandroluz97 <leandrodaluz97@gmail.com>
2023-07-10 22:52:49 +0100

    empty files

--- FinnStock.Frontend/src/providers/index.tsx
+++ FinnStock.Frontend/src/providers/index.tsx
@@ -1,15 +1,15 @@
 import React, { ReactNode } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import { QueryClientProvider } from 'react-query';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-import { HelmetProvider } from 'react-helmet-async';
+// import { ErrorBoundary } from 'react-error-boundary';
+// import { QueryClientProvider } from 'react-query';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import { ToastContainer } from 'react-toastify';
+// import { HelmetProvider } from 'react-helmet-async';
 
-import { queryClient } from '../lib/react-query';
-import { AuthProvider } from '../lib/auth';
-import { URLSearch, urlQueries } from '../utils/URLSearch';
-import { ErrorFallback } from '../components/Error';
-import { Loading } from '../components/Loading/Loading';
+// import { queryClient } from '../lib/react-query';
+// import { AuthProvider } from '../lib/auth';
+// import { URLSearch, urlQueries } from '../utils/URLSearch';
+// import { ErrorFallback } from '../components/Error';
+// import { Loading } from '../components/Loading/Loading';
 
 URLSearch.register(urlQueries);
 interface IPropsAppProvider {
@@ -18,17 +18,18 @@ interface IPropsAppProvider {
 
 export const AppProvider = ({ children }: IPropsAppProvider) => {
     return (
-        <ErrorBoundary FallbackComponent={ErrorFallback}>
-            <HelmetProvider>
-                <React.Suspense fallback={<Loading />}>
-                    <QueryClientProvider client={queryClient}>
-                        <ToastContainer />
-                        <AuthProvider>
-                            <Router>{children}</Router>
-                        </AuthProvider>
-                    </QueryClientProvider>
-                </React.Suspense>
-            </HelmetProvider>
-        </ErrorBoundary>
+        // <ErrorBoundary FallbackComponent={ErrorFallback}>
+        //     <HelmetProvider>
+        //         <React.Suspense fallback={<Loading />}>
+        //             <QueryClientProvider client={queryClient}>
+        //                 <ToastContainer />
+        //                 <AuthProvider>
+        //                     <Router>{children}</Router>
+        //                 </AuthProvider>
+        //             </QueryClientProvider>
+        //         </React.Suspense>
+        //     </HelmetProvider>
+        // </ErrorBoundary>
+        <h1>Hello world</h1>
     );
 };
