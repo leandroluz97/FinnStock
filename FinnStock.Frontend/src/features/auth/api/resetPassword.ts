dafe673df6c3ec90fcf69ff028ba096f10ce9751
leandroluz97 <leandrodaluz97@gmail.com>
2023-07-16 23:13:03 +0100

    removed unsused restpassword api

--- FinnStock.Frontend/src/features/auth/api/resetPassword.ts
+++ FinnStock.Frontend/src/features/auth/api/resetPassword.ts
@@ -1,9 +1,4 @@
-import { useMutation } from 'react-query';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import { axios } from '../../../lib/axios';
-import { MutationConfig, queryClient } from '../../../lib/react-query';
-import { toasterConfig } from '../../../lib/react-toastify';
 
 export type ResetPasswordDto = {
     data: {
@@ -17,19 +12,19 @@ export const resetPassword = ({ data }: ResetPasswordDto): Promise<any> => {
     return axios.post('/auth/ResetPassword', data);
 };
 
-type UseResetPasswordDto = {
-    config?: MutationConfig<typeof resetPassword>;
-};
+// type UseResetPasswordDto = {
+//     config?: MutationConfig<typeof resetPassword>;
+// };
 
-export const useResetPassword = ({ config }: UseResetPasswordDto = {}) => {
-    return useMutation({
-        onError: (_, __, context: any) => {
-            toast.error('Error Reset Password', toasterConfig);
-        },
-        onSuccess: () => {
-            queryClient.invalidateQueries('auth');
-        },
-        ...config,
-        mutationFn: resetPassword,
-    });
-};
+// export const useResetPassword = ({ config }: UseResetPasswordDto = {}) => {
+//     return useMutation({
+//         onError: (_, __, context: any) => {
+//             toast.error('Error Reset Password', toasterConfig);
+//         },
+//         onSuccess: () => {
+//             queryClient.invalidateQueries('auth');
+//         },
+//         ...config,
+//         mutationFn: resetPassword,
+//     });
+// };
