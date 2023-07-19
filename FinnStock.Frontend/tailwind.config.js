a32f48427d58b657be507955c301528e5fc664bd
leandroluz97 <leandrodaluz97@gmail.com>
2023-07-19 02:40:16 +0100

    change html css path

--- FinnStock.Frontend/tailwind.config.js
+++ FinnStock.Frontend/tailwind.config.js
@@ -4,7 +4,7 @@ import colors from 'tailwindcss/colors';
 /** @type {import('tailwindcss').Config} */
 
 export default {
-    content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
     theme: {
         extend: {
             colors: {
