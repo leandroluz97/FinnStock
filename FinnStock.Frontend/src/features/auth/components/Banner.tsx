75b3c84222882affc1378bd748d1c389a2f9f459
leandroluz97 <leandrodaluz97@gmail.com>
2023-07-21 00:46:58 +0100

    added auth background image

--- FinnStock.Frontend/src/features/auth/components/Banner.tsx
+++ FinnStock.Frontend/src/features/auth/components/Banner.tsx
@@ -10,10 +10,12 @@ interface BannerProps {
 export const Banner = ({ title, content }: BannerProps) => {
     return (
         <div
-            className={`hidden md:block flex-1 bg-primary-900 p-10 bg-[url('${logo}')] bg-no-repeat bg-bottom bg-contain`}
+            className="hidden md:block flex-1 bg-primary-900 p-10 bg-no-repeat bg-bottom bg-contain"
+            style={{ backgroundImage: `url("${logo}")` }}
         >
             <div>
                 <img src={finnstockLogo} alt="finnstock" />
+                {/* <img src={logo} alt="finnstock" /> */}
             </div>
             <div className="mt-16">
                 <h1 className="text-white text-3xl font-medium pb-3">{title}</h1>
